3ac61f18f995c6be5a560cf49be5a7181126bec9
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2022-10-22 17:11:46 -0700

    Changed pip in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,6 @@ set -e
 
 PROJECT_NAME="python_starter"
 PYTHON_VERSION="3.10.4"
-PIP="python3 -m pip"
 
 # Use project root as working directory
 cd "$(dirname -- "$0";)/.." || exit 1
@@ -19,20 +18,15 @@ if ! pyenv virtualenv --version; then
   exit 1
 fi
 
-if ! $PIP --version; then
-  echo "Install pip"
-  exit 1
-fi
-
 pyenv install --skip-existing $PYTHON_VERSION
 pyenv local $PYTHON_VERSION
 pyenv uninstall -f $PROJECT_NAME
 pyenv virtualenv $PYTHON_VERSION $PROJECT_NAME -f
 pyenv local $PROJECT_NAME
 
-$PIP install --upgrade pip
-$PIP install wheel
-$PIP install -r requirements.txt
+pip install --upgrade pip
+pip install wheel
+pip install -r requirements.txt
 
 pyenv rehash
 
